cfd7b1f81e0b84df8393c0e3b9682f1530a4632d
Brandon Davis <brandon.davis9@gmail.com>
2023-10-21 15:49:34 -0400

    Initial Commit

---
+++ background.py
@@ -0,0 +1,34 @@
+"""
+POK Bible Verse: Matthew 18:6
+CASTLE/HOUSEHOLD/KINGDOM OF GOD DEFENSE GAME FOR BOYS
+1 Timothy 5:8
+-------------------------------------------------------
+What is the goal of this game?
+-Protect your wife, then family, then community of believes against foreign attackers
+using various methods of castle defense.
+-------------------------------------------------------
+What types of methods can you use to protect your dependants?
+-Iron Pillars (Jeremiah 1:18)
+-Bronze Walls (Jeremiah 1:18)
+-Garrisons (2 Chornicles 17:2)
+-Belt of Truth (Eph 6:14)
+-Sword of the Spirit (Ephesians 6:17)
+-Feet of Peace (Eph 6:15)
+-Helmet of Salvation (Eph 6:17)
+-Breastplate of Righteousness (Eph 6:14)
+-Shield of Faith (Eph 6:16)
+-Shield of Salvation (Psalm 18:35)
+-Bronze bow (Psalm 18:34)
+-------------------------------------------------------
+What types of attackers are there?
+-Arameans (2 Chronicles 18)
+-Witches (Exodus 22:18)
+-Warlocks (Leviticus 20:27)
+-Wizard (Deuteronomy 18:10)
+-Demons (Matthew 8:28)
+-Sorcerers (2 Chronicles 33:6)
+-Mediums (2 Chronicles 33:6)
+-Necromancers (2 Chronicles 33:6)
+-------------------------------------------------------
+-can maybe buy tower defense asset packs. would make my life MUCH easier
+"""
